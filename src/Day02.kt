5a38d5b9fde3e7e951f351d407d61b9bedf1cb5a
asm0dey <pavel.finkelshtein@gmail.com>
2023-12-02 15:57:20 +0100

    Day 2

---
+++ src/Day02.kt
@@ -0,0 +1,39 @@
+fun main() {
+    fun part1(input: List<String>): Int = input
+        .asSequence()
+        .map { it.split(',', ' ', ':', ';').filterNot { it.isBlank() }.chunked(2) }
+        .map {
+            it.first()[1].toInt() to it.drop(1)
+        }
+        .filter { (_, dice) ->
+            dice.all { (num, color) ->
+                when (color) {
+                    "red" -> num.toInt() <= 12
+                    "green" -> num.toInt() <= 13
+                    "blue" -> num.toInt() <= 14
+                    else -> false
+                }
+            }
+        }
+        .sumOf { it.first }
+
+    fun part2(input: List<String>): Long = input
+        .asSequence()
+        .map { it.split(',', ' ', ':', ';').filterNot { it.isBlank() }.chunked(2) }
+        .map {
+            it.first()[1].toInt() to it.drop(1)
+        }
+        .map { (_, games) ->
+            games.filter { it[1] == "red" }.maxOf { it[0].toInt() }.toLong() *
+                    games.filter { it[1] == "green" }.maxOf { it[0].toInt() }.toLong() *
+                    games.filter { it[1] == "blue" }.maxOf { it[0].toInt() }.toLong()
+        }
+        .sum()
+
+    val test = readInput("02t1")
+    part1(test).println()
+    val input = readInput("02")
+    part1(input).println()
+    part2(test).println()
+    part2(input).println()
+}
